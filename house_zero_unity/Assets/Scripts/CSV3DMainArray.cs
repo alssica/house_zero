51bafa6924ddc5ebf5ec0e61594fa47f30f2ccb3
alssica <alssica@gmail.com>
2017-07-04 14:24:03 -0400

    CFD 3D + Deployment Test
    - implemented 3D CFD model with ability to move CFD section cut with camera position
    - deployment test of what we have
    problem to fix:
    1. CFD dynamic 3D data overwhelms hololens performance (head position constantly changing) - put in CFD 2D static viz as a placeholder for now
    2. Heat map and CFD voice commands not responding
    3. Seonsor text in low res somehow
    4. Room orientation

---
+++ house_zero_unity/Assets/Scripts/CSV3DMainArray.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class CSV3DMainArray : MonoBehaviour {
+
+    //[SerializeField]
+
+    private List<csvData> data;
+    //csvData[] dataCFD;
+    //private List<float> Vmag = new List<float>();
+    //float[] Vmag;
+    static Material lineMaterial;
+    public float offsetX = 8f;
+    public float offsetY = 14.5f;
+    public float offsetZ = 32f, obsDist = 8f;
+    private float vLenMin = 0.25f;
+    public float vLen = 0.65f;
+    private float vmagMin, vmagMax;
+    public bool showCFD;
+
+    void Awake()
+    {
+        data = CSVReaderClass.Read("3d_field");                      //put csv file in the resources folder to read
+    }
+
+    // Use this for initialization
+    void Start () {
+        /*
+        foreach(csvData c in dataCFD)
+        {
+            Vmag.Add(GetVmag(c));
+        }
+
+        vmagMin = Vmag.Min();
+        vmagMax = Vmag.Max();
+        */
+    }
+
+    void OnCFDToggle()
+    {
+        if (showCFD == false)
+        {
+            showCFD = true;
+        }
+        else
+        {
+            showCFD = false;
+        }
+    }
+
+    void Update()
+    {
+        /*
+         if (showCFD == true)
+         {
+             var cameraPos = Camera.main.transform.position;
+             print(cameraPos);
+
+             for (var i = 0; i < data.Count(); i++)
+             {
+                 var c = data[i];
+                 if (c.v0.z - offsetZ - 0.1f < Camera.main.transform.position.z || Camera.main.transform.position.z < c.v0.z - offsetZ + 0.1f)
+                 {
+                     dataCFD[i] = c;
+                     Vmag[i] = (GetVmag(c));
+                 }
+             }
+             print(dataCFD.Length);
+             print(Camera.main.transform.position.z);
+
+             vmagMin = Vmag.Min();
+             vmagMax = Vmag.Max();      
+           }
+        */
+    }
+
+    public List<csvData> updateCFD()
+    {
+        var cameraPos = Camera.main.transform.position;
+
+        List<csvData> newCFD = new List<csvData>();
+
+        foreach (csvData c in data)
+        {
+            if (c.v0.z - offsetZ - 0.1f < Camera.main.transform.position.z && Camera.main.transform.position.z < c.v0.z - offsetZ + 0.1f)
+            //if (c.v0.z - offsetZ == Camera.main.transform.position.z)
+            {
+                newCFD.Add(c);  
+            }
+        }
+        //print(newCFD.Count);
+        return (newCFD);
+    }
+
+    void updateMag()
+    {
+        List<float> Vmag = new List<float>();
+        foreach (csvData CFD in updateCFD())
+        {
+            Vmag.Add(GetVmag(CFD));
+        }
+
+        vmagMin = Vmag.Min();
+        vmagMax = Vmag.Max();
+    }
+    
+
+    public void OnRenderObject()                                        //rendering hirarchy: after all other renderings are done
+    {
+        if (showCFD == true)
+        {
+            updateCFD();
+            updateMag();
+
+            CreateLineMaterial();
+            lineMaterial.SetPass(0);
+
+            GL.PushMatrix();
+            //GL.MultMatrix(transform.worldToLocalMatrix);                  //make it follow the camera:
+
+            //draw lines
+            GL.Begin(GL.LINES);
+
+            foreach (csvData CFD in updateCFD())
+            {
+
+                float a = HeatMap.remap(CFD.vmag, vmagMin, vmagMax, 0, 1);
+                Color Vcolor = new Color(0.85f * a, 1 - a, 0, 0.5f);
+                GL.Color(Vcolor);
+
+                //GL.Vertex3(Mathf.Cos(angle) * radius, Mathf.Sin(angle) * radius, 0);
+                var vLenMax = vLenMin = vLen;
+                float r = HeatMap.remap(CFD.vmag, vmagMin, vmagMax, vLenMin, vLenMax);
+                Vector3 newVT = CFD.v0 + r * CFD.vvec;
+                GL.Vertex3(CFD.v0.x - offsetX, CFD.v0.y - offsetY, CFD.v0.z - offsetZ-obsDist);
+                GL.Vertex3(newVT.x - offsetX, newVT.y - offsetY, newVT.z - offsetZ-obsDist);
+            }
+
+            GL.End();
+            GL.PopMatrix();
+        }
+    }
+
+    static void CreateLineMaterial()
+    {
+        if (!lineMaterial)
+        {
+            Shader lineShader = Shader.Find("Hidden/Internal-Colored");
+            lineMaterial = new Material(lineShader);
+            lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+
+            //shader config: add alpha blending, turn off back face cull and depth writes
+            lineMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            lineMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            lineMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+            lineMaterial.SetInt("_ZWrite", 0);
+        }
+    }
+
+    private static float GetVmag(csvData c)
+    {
+        return c.vmag;
+    }
+}
---
+++ house_zero_unity/Assets/Scripts/SpeechManager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.Windows.Speech;
+
+public class SpeechManager : MonoBehaviour {
+
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+	// Use this for initialization
+	void Start () {
+
+        //open close
+        keywords.Add("Open", () =>
+        {
+            var focusObject = GazeGestureManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                focusObject.SendMessage("OnOpen");
+            }
+        });
+
+        keywords.Add("Close", () =>
+        {
+            var focusObject = GazeGestureManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                focusObject.SendMessage("OnClose");
+            }
+        });
+
+        //controls the width of opening
+        keywords.Add("A Quarter Open", () =>
+        {
+            var focusObject = GazeGestureManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                focusObject.SendMessage("OnQuarter");
+            }
+        });
+
+        keywords.Add("Half Open", () =>
+        {
+            var focusObject = GazeGestureManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                focusObject.SendMessage("OnHalf");
+            }
+        });
+
+        keywords.Add("Three Quarters Open", () =>
+        {
+            var focusObject = GazeGestureManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                focusObject.SendMessage("OnThreeQuarters");
+            }
+        });
+
+        //calls for UI panel to appear
+        keywords.Add("Dashboard", () =>
+        {
+            var UIobj = TriggerUI.TriggerObj;
+            UIobj.SendMessage("OnDashboard");
+        });
+
+        keywords.Add("Hide", () =>
+        {
+            var UIobj = TriggerUI.TriggerObj;
+            UIobj.SendMessage("OnHide");
+
+            var UIpop = GameObject.Find("UIpop");
+            UIpop.SendMessage("OnHide");
+
+            //var UIpopout = UIPop.TriggerObj;
+            //UIpopout.SendMessage("OnHide");
+        });
+
+        keywords.Add("Heat Map", () =>
+        {
+            var floor = GameObject.Find("Quad");
+            floor.SendMessage("OnToggleHM");
+        });
+
+        keywords.Add("Air Flow", () =>
+        {
+            Camera.main.SendMessage("OnCFDToggle");
+        });
+
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+	
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+}
